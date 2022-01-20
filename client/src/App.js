dcb9bf3b302732b2c6d24fb16e717f986b94e549
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-20 11:03:15 -0500

    working on single thought route

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {
 	InMemoryCache,
 	createHttpLink,
 } from "@apollo/client";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -17,7 +17,7 @@ import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 
 const httpLink = createHttpLink({
-	uri: "graphql", // uniform resourse identifier
+	uri: "/graphql", // uniform resourse identifier
 });
 
 const client = new ApolloClient({
@@ -32,11 +32,14 @@ function App() {
 				<div className="flex-column justify-flex-start min-100-vh">
 					<Header />
 					<div className="container">
-						<Route exact path="/" component={Home} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/signup" component={Signup} />
-						<Route exact path="/profile" component={Profile} />
-						<Route exact path="/thought" component={SingleThought} />
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/login" component={Login} />
+							<Route exact path="/signup" component={Signup} />
+							<Route exact path="/profile/:username?" component={Profile} />
+							<Route exact path="/thought/:id" component={SingleThought} />
+							<Route component={NoMatch} />
+						</Switch>
 					</div>
 					<Footer />
 				</div>
